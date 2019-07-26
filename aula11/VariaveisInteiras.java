0a37c4ab61a4054553ebfb3c8d3b92be27dd1efd
JoaoPedriali <53233703+JoaoPedriali@users.noreply.github.com>
2019-07-26 13:02:19 -0300

    Aulas 6 - 12

---
+++ aula11/VariaveisInteiras.java
@@ -0,0 +1,17 @@
+package com.joao.cursojava.aula11;
+
+public class VariaveisInteiras
+{
+    public static void main (String[] args)
+    {
+        byte idade01 = 21;
+        short idade02 = 22;
+        int idade03 = 23;
+        long idade04 = 24;
+
+        System.out.println("Valor variável idade01: " + idade01);
+        System.out.println("Valor variável idade01: " + idade02);
+        System.out.println("Valor variável idade01: " + idade03);
+        System.out.println("Valor variável idade01: " + idade04);
+    }
+}
---
+++ aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,13 @@
+package com.joao.cursojava.aula11;
+
+public class VariaveisPontoFlutuante
+{
+    public static void main(String[] args)
+    {
+            double valorPassagem = 4.50;
+            float valorTomate = 6.50f;
+
+            System.out.println("Valor da passagem: " + valorPassagem);
+            System.out.println("Valor do tomate: " + valorTomate);
+    }
+}
